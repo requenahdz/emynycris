0b060cbc2ab97e77f4a057c8f148c0403145a4e2
Roberto Requena <rrequena@acsyt.com>
2026-04-20 20:19:30 -0600

    fix url mapa

--- src/components/sections/InvitationSection.tsx
+++ src/components/sections/InvitationSection.tsx
@@ -1,9 +1,16 @@
 export default function InvitationSection() {
+  
+ const mapsUrl = "https://maps.app.goo.gl/EmFZtMGeXgBmAKkAA";
+
   return (
     <section className="relative min-h-screen w-full overflow-hidden flex items-center justify-center px-2 py-6 sm:px-4 sm:py-10">
       <div
         className="absolute inset-0"
-        style={{ backgroundImage: "url(/assets/images/fondo-2.png)", backgroundRepeat: "repeat", backgroundSize: "720px auto" }}
+        style={{
+          backgroundImage: "url(/assets/images/fondo-2.png)",
+          backgroundRepeat: "repeat",
+          backgroundSize: "720px auto",
+        }}
       />
 
       <img
@@ -13,7 +20,21 @@ export default function InvitationSection() {
       />
 
       <div className="relative z-10 w-full md:max-w-[720px] lg:max-w-[820px]">
-        <img src="/assets/images/invi.png" alt="Invitación" className="w-full h-auto" />
+        <div className="relative w-full">
+          <img
+            src="/assets/images/invi.png"
+            alt="Invitación"
+            className="w-full h-auto block"
+          />
+
+          <a
+            href={mapsUrl}
+            target="_blank"
+            rel="noreferrer"
+                   className="absolute left-1/2 bottom-[3%] z-20 h-[15%] w-full -translate-x-1/2"
+            aria-label="Abrir ubicación"
+          />
+        </div>
       </div>
     </section>
   );
